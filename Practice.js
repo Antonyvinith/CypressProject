22f081722266b3d69979430486aae444e304cee3
Antony Vinith <antony.vinith@pivotree>
2024-08-13 14:24:26 +0530

    commit

--- Practice.js
+++ Practice.js
@@ -1,7 +1,39 @@
-var a="hello world hello";
-
-for(e of a){
-    if(e==' '){
-        console.log("asd")
+var a="   fly me   to   the moon  ";
+a=a.trim();
+var length=0
+for(let i=a.trim().length-1;i>=0;i--){
+    if(a[i]!=' '){
+        length++;
     }
+    else{
+        break;
+    }
+    
+
+
 }
+
+
+// console.log(Math.ceil(Math.pow(8,0.5)))
+    
+function sqrt(number) {
+    if (number < 0) return NaN; 
+    if (number === 0 || number === 1) return number;
+
+    let low = 0;
+    let high = number;
+    let mid;
+    let tolerance = 0.000001; 
+
+    while (high - low > tolerance) {
+        mid = (low + high) / 2;
+        if (mid * mid > number) {
+            high = mid;
+        } else {
+            low = mid;
+        }
+    }
+
+    return (low + high) / 2;
+}
+
